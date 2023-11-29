8b030899c83e72693b0a3cef6ef6832f95fe47ae
bcjtrf <regrowth1521@gmail.com>
2023-11-29 21:16:33 +0800

    Add files via upload
    use of pyperclip

---
+++ # Import the pyperclip module.py
@@ -0,0 +1,12 @@
+# Import the pyperclip module
+import pyperclip
+
+# Define the text to be copied
+text = "h8uf99w<b7nf9W3"
+
+# Copy the text to the clipboard
+pyperclip.copy(text)
+
+
+# Print a confirmation message
+print("The text has been copied to your keyboard.")
